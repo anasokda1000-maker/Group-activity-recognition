69c34bb06577264b9a3f7703acdb33f50d355368
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-09 06:38:22 +0300

    Update Baseline-3.py

--- models./Baseline-3.py
+++ models./Baseline-3.py
@@ -7,16 +7,32 @@ class Baseline_B3_tuned(nn.Module):
         super().__init__()
         self.backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
 
-        self.backbone.fc = nn.Sequential(
-            nn.Linear(self.backbone.fc.in_features, 1024),
+        in_features = self.backbone.fc.in_features
+
+        self.backbone.fc = nn.Identity()
+        
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(1024, 9)
+            nn.Dropout(p=0.5),
+            nn.Linear(1024, 8),
         )
 
     def forward(self, x):
-            B, C, H, W = x.shape
+        B, P, C, H, W = x.shape
+
+        x = x.view(B * P, C, H, W)
+
+        features = self.backbone(x)
+
+        features = features.view(B, P, -1)
+
+        pooled_features, _ = torch.max(features, dim=1)
+
+        output = self.classifier(pooled_features)
         
-            features = self.backbone(x)  # [B, 9]
-    
-            return features
+        return output # tensor.size([B, 8])
