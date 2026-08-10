843c014275017686df5afdd6ed0ec942896f73cd
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-10 19:17:04 +0300

    Update Baseline_7.py

--- trainers/Baseline_7.py
+++ trainers/Baseline_7.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
     
     val_loader = DataLoader(
         val_dataset, 
-        batch_size=['Baseline_7']['model']['batch_size'], 
+        batch_size=Configs['Baseline_7']['model']['batch_size'], 
         shuffle=False, 
         num_workers=4,
     )
