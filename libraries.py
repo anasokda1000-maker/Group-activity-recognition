e1685f4b79dd937e4bd66fc7cab227f58ffae0be
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-10 16:33:30 +0300

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,1 +1,31 @@
+# Standard Library
+import csv
+import gc
+import os
+import pickle
+import random
+import sys
+import zipfile
+from pathlib import Path
 
+
+# Third-Party Libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import psutil
+import seaborn as sns
+from PIL import Image
+from tqdm import tqdm
+
+
+# PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+from torchvision.models import ResNet50_Weights, resnet50
+
+# Sklearn
+from sklearn.metrics import confusion_matrix, f1_score
