a44d5adc45026a25ec50e280450fe64382d3dd90
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-10 19:09:48 +0300

    Update Baseline_7.py

--- trainers/Baseline_7.py
+++ trainers/Baseline_7.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
         num_workers=4,
     )
     
-    device = torch.load(Configs['device'])
+    device = torch.device(Configs['device'])
     model = Baseline_7()
     name_map = {'conv1': '0', 'bn1': '1', 'layer1': '4', 'layer2': '5', 'layer3': '6', 'layer4': '7'}   
     remapped_weights = {}
