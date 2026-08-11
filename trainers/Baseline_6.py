effc556c0ae8bbd36e04de1a35d60ffa9eab2ba9
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-11 13:14:14 +0300

    Update Baseline_6.py

--- trainers/Baseline_6.py
+++ trainers/Baseline_6.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from libraries import *
 from utils import *
 from models.Baseline_6 import Baseline_6
