824a5d23f66be1e5ad5e39b4227ca85351db63c5
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-11 12:26:37 +0300

    Update trainer.py

--- fine_tuning/trainer.py
+++ fine_tuning/trainer.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
         Enviroment['videos_root'],
         Enviroment['videos_tracking_annot'],
         Enviroment['working_root'],
-        train_data=Configs['data']['train_ids']
+        train_data=Configs['data']['train_ids'],
         val_data=Configs['data']['val_ids']
     )
     
