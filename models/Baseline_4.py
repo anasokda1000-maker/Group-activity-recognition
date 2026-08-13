6e18cd6fa2b9f27914913a16675e78a46cbcee7e
anasokda1000-maker <anasokda1000@gmail.com>
2026-08-13 16:02:22 +0300

    Update Baseline_4.py

--- models/Baseline_4.py
+++ models/Baseline_4.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 
 class Baseline_4(nn.Module):
+    """
+    backbone : resnet50 
+    batch : clip
+    """
     def __init__(self):
         super().__init__()
         resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
@@ -21,13 +25,21 @@ class Baseline_4(nn.Module):
         
     def forward(self, x):
         B, S, C, H, W = x.shape
+        """
+        B : batch size
+        S : number of frames in the clip (9)
+        C : number of channels (3)
+        H : height (224)
+        W : width (224)
+        """
         x = x.view(B * S, C, H, W)
       
-        features = self.backbone(x)
+        features = self.backbone(x) # (B*S, 2048)
         features = features.view(B, S, -1)
       
-        lstm_out, _ = self.lstm(features)
+        lstm_out, _ = self.lstm(features) # (B*S, 1024)
+                                          # fed to lstm for the frames sequence dynamics 
       
         predicted = self.classifier(lstm_out[:, -1, :])
       
-        return predicted   # [B, num_classes]
+        return predicted   # [B, 8]
